0839226cba0ccc893e8c29e0105caacaeb59a4ed
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-22 17:37:01 -0400

    fixed wrong character

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+
+
 const { application } = require('express');
 
 const express = require('express')();
 const port = 3000;
 
-const bodyParser = require('express'),json;
+const bodyParser = require('express').json;
 application.use(bodyParser());
 
 application.listen(port, () => {
