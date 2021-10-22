e4325a22b5377bd3deecd100e30bd994adcb9986
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-22 17:12:24 -0400

    adding config DB code

--- config/db.js
+++ config/db.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connected");
+    })
+    .catch((err) => console.log(err));
