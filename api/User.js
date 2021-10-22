c2ca51f5c669e0470f713f153e8709e1f834f1f7
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-22 19:28:46 -0400

    added code to hash passwords

--- api/User.js
+++ api/User.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const User = require('../models/User');
 
+const bcrypt = require('bcrypt');
+
 //sign up route
 router.post('/signup', (req, res) => {
     let {name, email, password, dateOfBirth} = req.body;
@@ -44,7 +46,35 @@ router.post('/signup', (req, res) => {
                     message: "A User with this Email already exists!"
                 })
             } else {
+                const saltRounds = 10;
+                bcrypt.hash(password, saltRounds).then(hashedPassword => {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password: hashedPassword,
+                        dateOfBirth
+                    });
 
+                    newUser.save().then(result => {
+                        res.json({
+                            status: "Success!",
+                            message: "Signup Successful!",
+                            data: result,
+                        })
+                    })
+                    .catch(err => {
+                        res.json({
+                            status: "FAILED",
+                            message: "An Error ccured While Saving User Account!"
+                        })
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        status: "FAILED",
+                        message: "An Error ccured While Hashing the Password!"
+                    })
+                })
             }
         }).catch(err => {
             console.log(err);
