dc0a0f2e71e927a676a9d9fe4d3bd3db6ce2f42e
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-22 18:58:04 -0400

    added sign up for users code

--- api/User.js
+++ api/User.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
+const User = require('../models/User');
 
+//sign up route
+router.post('/signup', (req, res) => {
+    let {name, email, password, dateOfBirth} = req.body;
+    name = name.trim();
+    email = email.trim();
+    password = password.trim();
+    dateOfBirth = dateOfBirth.trim();
+
+    if (name == "" || email == "" || password == "" || dateOfBirth == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty Input Fields!"
+        });
+    } else if (!/^[a-zA-Z]*$/test(name)) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid Name Entered!"
+        })
+    } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid Email Entered!"
+        })    
+    } else if (!new Date(dateOfBirth).getTime()) {
+        res.json({
+            status: "FAILED",
+            message: "Invalid Date of Birth Entered!"
+        })
+    } else if (password.length < 8) {
+        res.json({
+            status: "FAILED",
+            message: "Password is too Short!"
+        })
+    } else if {
+
+    }
 })
 
+//sign in route
 router.post('/signin', (req, res) => {
     
 })
