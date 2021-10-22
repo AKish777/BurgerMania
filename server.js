4384b44471750cdb4dd544c234f15ab9ad81aaad
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-22 17:19:48 -0400

    made adjustments to server file

--- server.js
+++ server.js
@@ -1,42 +1,11 @@
-const express = require('express')
-const path = require('path')
-const mysql = require('mysql')
-const dotenv = require('dotenv')
+const { application } = require('express');
 
-dotenv.config({ path: './.env'})
+const express = require('express')();
+const port = 3000;
 
-const app = express()
+const bodyParser = require('express'),json;
+application.use(bodyParser());
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+application.listen(port, () => {
+    console.log(`Server Running on Port ${port}`);
 })
-
-const publicDirectory = path.join(__dirname, './public')
-app.use(express.static(publicDirectory))
-
-app.use(express.urlencoded({ extended: false }))
-
-app.use(express.json())
-
-app.set('view engine', 'hbs')
-
-db.connect( (error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected...")
-    }
-})
-
-const bcrypt = require('bcrypt')
-
-const users = []
-
-app.use('/', require('./routes/pages'))
-
-app.use('/auth', require('./routes/auth'))
-
-app.listen(3000)
