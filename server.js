3018d80bb415df04012d0c8f67928f7e7514b99a
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-24 00:28:24 -0400

    forgot to save server file code

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ var app = express();
 
 app.use(express.static("public"));
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
 
 app.listen(PORT, function() {
 
