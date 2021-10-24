2bce1cf0d02fb5db6aa8d46eabe8462d8d309616
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-23 23:41:57 -0400

    added function code

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,13 @@
 var connection = require("../config/connection.js");
 
-
-
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
 module.exports = orm;
