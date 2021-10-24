e2d71d0f9bade1cddb5798d76881c09d10cd2466
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-23 23:40:18 -0400

    setting up connection to MYSQL

--- config/connection.js
+++ config/connection.js
@@ -10,3 +10,13 @@ if(process.env.MONGODB_URL){
     database: 'burgers_db'
   })
 }
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
