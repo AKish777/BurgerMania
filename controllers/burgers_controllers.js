031be2dc7e4524d3d81c000933613d433e100219
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-23 23:47:21 -0400

    setting up controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,4 +3,13 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res){
+    burger.select(function(data){
+        var burgerOb = {
+            burgers: data
+        };
+        res.render("index", burgerOb)
+    });
+});
+
 module.exports = router;
