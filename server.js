5754ccd4c9aa4e0613989e33410a05b86e2c9cf5
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-23 23:31:42 -0400

    finished port setup

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ var app = express();
 
 app.use(express.static("public"));
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+
+  console.log("Server listening on: http://localhost:" + PORT);
+});
