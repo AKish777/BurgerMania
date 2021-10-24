7a5507f0649fa5b9e8837c2f7458144a784a8bfb
Alex Kichigin <alexkichigin@Alexs-MacBook-Pro.local>
2021-10-23 23:40:01 -0400

    setting up connection to MYSQL

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,12 @@
 var mysql = require("mysql");
+
+if(process.env.MONGODB_URL){
+  connection = mysql.createConnection(process.env.MONGODB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'root',
+    database: 'burgers_db'
+  })
+}
